eff6a2c4576c84fda6e3939b6902876a0b274196
MelanieBallesteros <129481165+MelanieBallesteros@users.noreply.github.com>
2023-09-08 20:37:56 +1000

    new class f1-f4 done

--- StudentMarksAnalyzer.java
+++ StudentMarksAnalyzer.java
@@ -5,13 +5,53 @@
  * @author (Melanie Ballesteros)
  * @version (a version number or a date)
  */
+
+
+import java.util.Scanner;
+
+
 public class StudentMarksAnalyzer
 {
-   public static void main (String[] args){
-       Scanner scanner = new Scanner(System.in);
+   //double[] studentMarks = new double[30];
+   
+    public static void main (String[] args){
+       int[] marks = new int[30];
+       int studentCount = 0;
        
-       double[] studentMarks = new double[30]
-       
-       
-   }
+       Scanner scanner = new Scanner(System.in); //Create a Scanner object to read input from the user
+       System.out.print("Please enter the assignment name: "); //Input the assignment name
+       String assignmentName = scanner.nextLine();
+       System.out.println("Assignment Name entered is: " + assignmentName);
+    
+       while (studentCount < 30) {
+           System.out.print("Enter mark for student " + (studentCount + 1) + " : ");
+           int mark = scanner.nextInt();
+           
+           if (mark >= 0 && mark <= 30){
+               marks[studentCount] = mark;
+               studentCount++;
+            }
+            else {
+                System.out.println("Invalid mark input! Mark must be between 0 and 30. Please enter a valid mark.");
+                
+            }
+           }
+    //Display the entered marks
+    
+    System.out.println("Entered marks for students: ");
+    for (int i = 0; i < 30; i++){
+        System.out.println("Student " + (i + 1) + " : " + marks[i]);
+        
+    }
+    
 }
+}
+       
+       
+       
+       
+      
+       
+       
+  // }
+//}
