8a5bb770167a90137331ba34fdfd8729006bb373
MelanieBallesteros <129481165+MelanieBallesteros@users.noreply.github.com>
2023-09-08 15:27:44 +1000

    created another class with main

---
+++ StudentMarksAnalyzer.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class StudentMarksAnalyzer here.
+ *
+ * @author (Melanie Ballesteros)
+ * @version (a version number or a date)
+ */
+public class StudentMarksAnalyzer
+{
+   public static void main (String[] args){
+       Scanner scanner = new Scanner(System.in);
+       
+       double[] studentMarks = new double[30]
+       
+       
+   }
+}
