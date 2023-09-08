dbd810cb65893161dfff868a5b1b4200ceeff0c7
MelanieBallesteros <129481165+MelanieBallesteros@users.noreply.github.com>
2023-09-08 21:19:05 +1000

    update on F5

--- StudentMarksAnalyzer.java
+++ StudentMarksAnalyzer.java
@@ -15,7 +15,6 @@ public class StudentMarksAnalyzer
    //double[] studentMarks = new double[30];
    
     public static void main (String[] args){
-       //int[] marks = new int[30];
        int[] studentMarks = new int[30];
        int studentCount = 0;
        
@@ -25,11 +24,10 @@ public class StudentMarksAnalyzer
        System.out.println("Assignment Name entered is: " + assignmentName);
     
        while (studentCount < 30) {
-           System.out.print("Enter mark for student " + (studentCount + 1) + " : ");
+           System.out.print("Enter mark for Student " + (studentCount + 1) + " : ");
            int mark = scanner.nextInt();
            
            if (mark >= 0 && mark <= 30){
-               //marks[studentCount] = mark;
                studentMarks[studentCount] = mark;
                studentCount++;
             }
@@ -38,31 +36,29 @@ public class StudentMarksAnalyzer
                 
             }
            }
-    //Display the entered marks
     
-    System.out.println("Entered marks for students ");
+    //Display the entered marks
+    System.out.println("Entered marks for students in " + assignmentName);
     for (int i = 0; i < 30; i++){
-        System.out.println("Student " + (i + 1) + " : " + marks[i]);
-        
+          System.out.println("Student " + (i + 1) + " : " + studentMarks[i]);
     }
     
     // Find the highest and lowest student marks
-    
-    int highestMark = marks[0];
-    int lowestMark = marks[0];
+    int highestMark = studentMarks[0];
+    int lowestMark = studentMarks[0];
     
     for (int i = 1; i < 30; i++){
-        if(marks[i] > highestMark){
-            highestMark = marks[i];
+        if(studentMarks[i] > highestMark){
+            highestMark = studentMarks[i];
         }
         
-        if(marks[i] < lowestMark) {
-            lowestMark = marks[i];
+        if(studentMarks[i] < lowestMark){
+           lowestMark = studentMarks[i];
         }
     }
     
-    //Display the entered highest and lowesr marks
-    
+    //Display the entered highest and lowes marks
+    System.out.println("Entered highest and lowest mark in " + assignmentName);
     System.out.println("Highest Mark: " + highestMark);
     System.out.println("Lowest Mark: " + lowestMark);
     
