3d85adbe90c752c25f6dcfeb1027e6e6f52083f0
MelanieBallesteros <129481165+MelanieBallesteros@users.noreply.github.com>
2023-09-10 21:29:45 +1000

    f5 still on progress

---
+++ __SHELL18.java
@@ -0,0 +1,10 @@
+
+public class __SHELL18 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+final bluej.runtime.BJMap __bluej_runtime_scope = getScope("C:\\Users\\acer\\Desktop\\MIT\\TERM 4\\PROG5001 Assessment 1\\PROG5001-Assessment-1");
+final StudentStatisticsMarks studentS1 = (StudentStatisticsMarks)__bluej_runtime_scope.get("studentS1");
+
+
+studentS1.StudentMarks();
+
+}}
