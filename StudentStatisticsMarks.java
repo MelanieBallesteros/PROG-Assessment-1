6e95d3bf604b6970cac41533b50a593f1848f931
MelanieBallesteros <129481165+MelanieBallesteros@users.noreply.github.com>
2023-09-11 10:58:51 +1000

    change int to double

--- StudentStatisticsMarks.java
+++ StudentStatisticsMarks.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 
 public class StudentStatisticsMarks
 {
-      int[]marks = new int[30]; //array to store marks
+      double[]marks = new double[30]; //array to store marks
      
     
      //Input assignment name
@@ -22,64 +22,85 @@ public class StudentStatisticsMarks
             System.out.println("Assignment Name entered is: " + assignmentName);
             scanner.close();
         }
- 
     
-    public void StudentMarks(){       
-        
+    
+    public void InputStudentMarks(){
+        Scanner scanner = new Scanner(System.in);
         for (int i = 0; i < 30; i++){
-            int studentNumber = i +1;
-            int mark = getInput(studentNumber);
+            System.out.print("Enter mark for Student " + (i + 1) + " : ");
+            double mark = scanner.nextDouble();
+        
+         //Check if the input is a valid mark (between 0 and 30)   
+        if (mark >= 0 && mark <= 30){
             marks[i] = mark;
+        }else
+        {
+            System.out.println("Invalid mark! Please enter a valid mark between 0 and 30.");
+                i--; // Decrement i to allow the user to re-enter the mark for the same student.
         }
+        
+    }
+         //Display the entered marks
+         
+         System.out.println("Entered marks for students " );
+         for (int i = 0; i < 30; i++){
+             System.out.println("Student " + (i +1)+ " :" + marks[i]);
+             
+         }
+        }
+        
+    }
+        
+        
+    
+//    public void StudentMarks(){       
+        
+//        for (int i = 0; i < 30; i++){
+//            int studentNumber = i +1;
+//            int mark = getInput(studentNumber);
+//            marks[i] = mark;
+//        }
+        
         
         //Display the entered marks
-        System.out.println("Entered marks for 30 students ");  
-        for (int i = 0; i<30; i++){
-            int studentNumber = i + 1;
-            int mark = marks[i];
-            System.out.println("Student " + studentNumber + " : " + mark);
-          }
-        }
+//        System.out.println("Entered marks for 30 students ");  
+//        for (int i = 0; i<30; i++){
+//            int studentNumber = i + 1;
+//            int mark = marks[i];
+//            System.out.println("Student " + studentNumber + " : " + mark);
+//          }
+                   
+          
+//        }
+        
         
         //Input student marks
-        public static int getInput(int studentNumber){
-            Scanner scanner = new Scanner(System.in);
-            int mark;            
+//        public static int getInput(int studentNumber){
+//            Scanner scanner = new Scanner(System.in);
+//            int mark;            
             
            // for(int i = 1; i <= 30; i++){
-            System.out.print("Enter mark for Student " + studentNumber + " : ");
-            mark=scanner.nextInt();
+//            System.out.print("Enter mark for Student " + studentNumber + " : ");
+//            mark=scanner.nextInt();
          
             //Display error message when invalid mark entered
-            if(mark < 0 || mark > 30){
-                System.out.println("Invalid mark input! Mark must be between 0 and 30. Please enter a valid mark for Student " + studentNumber + " : ");
-                mark=getInput(studentNumber); // Recurring call the method to re-enter a mark
-            }
-            return mark;           
-        }
+//            if(mark < 0 || mark > 30){
+//                System.out.println("Invalid mark input! Mark must be between 0 and 30. Please enter a valid mark for Student " + studentNumber + " : ");
+//                mark=getInput(studentNumber); // Recurring call the method to re-enter a mark
+//            }
+//            return mark;           
+//        }
+        //Method to find the lowest price
         
-    
+//        public double
+        
+        
+        
+        
+//    }
       
         
-       // Method to find and print the highest and lowest marks
-    public static void findAndPrintHighestAndLowestMarks(int[] marks) {
-        int highestMark = marks[0];
-        int lowestMark = marks[0];
-
-        for (int i = 1; i < marks.length; i++) {
-            if (marks[i] > highestMark) {
-                highestMark = marks[i];
-            }
-            if (marks[i] < lowestMark) {
-                lowestMark = marks[i];
-            }
-        }
-
-        System.out.println("Highest mark: " + highestMark);
-        System.out.println("Lowest mark: " + lowestMark);
-    }
-}
-
+      
 
 
 
